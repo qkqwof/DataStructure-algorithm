0580cd313c3553a02c295850316b3edc5a2177d5
qkqwof <qkqwof@yonsei.ac.kr>
2022-03-22 00:06:56 +0900

    algo

---
+++ 백준알고리즘/브루트 포스/일곱 난쟁이.py
@@ -0,0 +1,15 @@
+a = []
+for i in range(9):
+    a.append(int(input()))
+a.sort()
+
+result = sum(a)
+for i in range(9):
+    for j in range(i+1,9):
+        if result-a[i]-a[j] == 100:
+            for k in range(9):
+                if k == i or k == j:
+                    continue
+                else:
+                    print(a[k])
+            exit()
